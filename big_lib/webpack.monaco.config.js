93a9303334b0661cd2178af753dae6d499b4e9a9
Jan Święcki <jan.swiecki@gmail.com>
2020-04-16 17:43:04 +0200

    Update

--- big_lib/webpack.monaco.config.js
+++ big_lib/webpack.monaco.config.js
@@ -1,18 +1,13 @@
 const webpack = require('webpack');
 const path = require('path')
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-// const cwd = process.cwd()
-
 // For more info see this: https://github.com/microsoft/monaco-editor-webpack-plugin
 module.exports = {
-  // context: cwd,
 	mode: 'development',
   entry: {
-    // vendor: [path.resolve(__dirname, 'vendor_monaco')],
     vendor: ['./monaco'],
   },
   // optimization: {
@@ -20,17 +15,17 @@ module.exports = {
   //   minimizer: [new TerserPlugin()]
   // },
   output: {
+    // TODO make wiring for 'monaco_[hash].dll.js'
     filename: 'monaco.dll.js',
-    // path: path.resolve(cwd, 'dll/monaco/'),
     path: path.resolve(__dirname, '../dll/monaco/'),
-    library: 'vendor_monaco_lib',
+    library: '[name]_[hash]_dll',
 		libraryTarget: "window",
     publicPath: 'dll/monaco/'
   },
   plugins: [
     // new CleanWebpackPlugin(),
     new webpack.DllPlugin({
-      name: 'vendor_monaco_lib',
+      name: '[name]_[hash]_dll',
       path: path.resolve(__dirname, '../dll/monaco/manifest.json'),
     }),
     new MonacoWebpackPlugin({
